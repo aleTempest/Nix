855f1cad3782f058d305b48587d8423137c7ec83
ale <ale.edgedancer@gmail.com>
2025-05-06 20:25:52 -0600

    syncthing device id and folder declared

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -2,9 +2,20 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+
     settings.gui = {
       user = "ale";
       password = "elaina";
     };
+
+    devices = {
+      "POCOM6" = { id = "T5SWMAT-3VP52UE-WKFNP3O-3OBJ5VD-6AEOG56-VQR4LCR-XD5HHRD-2NARIQY"; };
+    };
+
+    folders = {
+      "Pictures" = {
+        path = "/home/ale/Pictures";
+        devices = [ "POCOM6" ];
+      };
   };
 }
